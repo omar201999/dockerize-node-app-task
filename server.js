8b06feb6bc46af49f3afefbfeaf8a63b53cac844
Omar Essam <46471716+omar201999@users.noreply.github.com>
2023-09-25 12:23:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Messi is the best player ever');
 });
 
 app.listen(PORT, HOST, () => {
